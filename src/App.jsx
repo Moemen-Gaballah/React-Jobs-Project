5fa248970bb967ed79857868293534d72edea9df
Moemen Gaballa <Moemen.gaballa@expertapps.com.sa>
2024-06-13 22:35:50 +0300

    init project react by vite

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+const App = () => {
+    const name = 'moemen';
+    return (
+        <>
+            <div className='text-5xl'>App</div>
+            <p>Hello {name}</p>
+        </>
+    );
+};
+export default App;
