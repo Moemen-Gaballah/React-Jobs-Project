a64659ae2f85ed499946c21935142cfd2ddd96a5
Moemen Gaballa <Moemen.gaballa@expertapps.com.sa>
2024-06-13 22:46:22 +0300

    use variable, array and expressions

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
 const App = () => {
     const name = 'moemen';
+    const names = ["moemen", "eslam", "ahmed", "mohsen"];
+    const loggedIn = true;
+
     return (
         <>
             <div className='text-5xl'>App</div>
             <p>Hello {name}</p>
+            <ul>
+                {
+                    names.map((name, index)=>(
+                    <li key={index}>{name}</li>
+                    ))
+                }
+            </ul>
+            {loggedIn && <h1>Hello Member</h1>}
         </>
     );
 };
