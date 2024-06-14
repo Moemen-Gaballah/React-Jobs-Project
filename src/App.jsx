ba173865658297f3f381a25f44e51ea81b3812d4
Moemen Gaballa <Moemen.gaballa@expertapps.com.sa>
2024-06-14 09:06:07 +0300

    install package react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
+import {Route, createBrowserRouter, createRoutesFromElements, RouterProvider} from 'react-router-dom';
 import Navbar from "./components/Navbar.jsx";
 import Hero from "./components/Hero.jsx";
 import HomeCards from "./components/HomeCards.jsx";
 import JobListings from "./components/JobListings.jsx";
 import ViewAllJobs from "./components/ViewAllJobs.jsx";
+
+const router = createBrowserRouter(
+    createRoutesFromElements(<Route index element={<h1>My App</h1>} />)
+);
 
 const App = () => {
     return <>
