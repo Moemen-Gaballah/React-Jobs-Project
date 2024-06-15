cffeb8de7217147b25e873f31b16187f0aa86e14
Moemen Gaballa <Moemen.gaballa@expertapps.com.sa>
2024-06-15 10:54:31 +0300

    init layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,17 @@
 import {Route, createBrowserRouter, createRoutesFromElements, RouterProvider} from 'react-router-dom';
-import Navbar from "./components/Navbar.jsx";
-import Hero from "./components/Hero.jsx";
-import HomeCards from "./components/HomeCards.jsx";
-import JobListings from "./components/JobListings.jsx";
-import ViewAllJobs from "./components/ViewAllJobs.jsx";
+import MainLayout from "./layouts/MainLayout";
+import HomePage from "./pages/HomePage.jsx";
 
 const router = createBrowserRouter(
-    createRoutesFromElements(<Route index element={<h1>My App</h1>} />)
+    createRoutesFromElements(
+        <Route path='/' element={<MainLayout/>}>
+            <Route index element={<HomePage/>}/>
+        </Route>
+    )
 );
 
 const App = () => {
-    return <>
-        <Navbar/>
-        <Hero title='Become a React Dev' subtitle='Find the React job that fits your skills and needs'/>
-        <HomeCards/>
-        <JobListings/>
-        <ViewAllJobs/>
-
-    </>;
+    return <RouterProvider router={router}/>;
 };
 
 export default App;
