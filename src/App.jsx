70214e28c6c98e4bb5d0de2287a07cd9ef3b3fd4
Moemen Gaballa <Moemen.gaballa@expertapps.com.sa>
2024-06-20 07:11:41 +0300

    delete job

--- src/App.jsx
+++ src/App.jsx
@@ -7,10 +7,8 @@ import JobPage, {jobLoader} from "./pages/JobPage.jsx";
 import AddJobPage from "./pages/AddJobPage.jsx";
 
 
-
 const App = () => {
     const addJob = async (newJob) => {
-        console.log(newJob);
         const res = await fetch('/api/jobs', {
             method: 'POST',
             headers: {
@@ -21,13 +19,22 @@ const App = () => {
         return;
     };
 
+    // Delete Job
+    const deleteJob = async (id) => {
+        const res = await fetch(`/api/jobs/${id}`, {
+            method: 'DELETE'
+        });
+
+        return;
+    };
+
     const router = createBrowserRouter(
         createRoutesFromElements(
             <Route path='/' element={<MainLayout/>}>
                 <Route index element={<HomePage/>}/>
                 <Route path='/jobs' element={<JobsPage/>}/>
                 <Route path='/add-job' element={<AddJobPage addJobSubmit={addJob}/>}/>
-                <Route path='/jobs/:id' element={<JobPage/>} loader={jobLoader}/>
+                <Route path='/jobs/:id' element={<JobPage deleteJob={deleteJob}/>} loader={jobLoader}/>
                 <Route path='*' element={<NotFoundPage/>}/>
             </Route>
         )
